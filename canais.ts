37b77f1493d04c1c8953078e2bdcc0297600c9dc
KodiTVR-add-on / AddonBarrosVR <43704963+KodiTVR-add-on@users.noreply.github.com>
2020-05-06 00:32:43 +0100

    Update canais.ts

--- canais.ts
+++ canais.ts
@@ -24,7 +24,7 @@
 
 
 <item>
-<title>A BOLA TV [HD]</title>
+<title>A BOLA TV PT [HD]</title>
  <link>plugin://plugin.video.f4mTester/?streamtype=HLSRETRY&amp;maxbitrate=0&amp;Buffer=1&amp;url=http://m3u.box365.live:8080/live/Ruidourado/Hh7782/826.m3u8</link>
 <item>
 
